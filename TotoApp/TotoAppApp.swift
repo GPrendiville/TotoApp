6706f67fa2c0c646abbcc2a1f53f21885fcd6361
GPrendiville <griffithprendiville@gmail.com>
2023-11-27 15:49:39 -0800

    Initial Commit

---
+++ TotoApp/TotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotoAppApp.swift
+//  TotoApp
+//
+//  Created by Griffith Prendiville on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
